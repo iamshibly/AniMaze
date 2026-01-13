84267d12b5282bdac1cf368d9cc4e2a10045e6a6
Md. Zubayer Ahmad Shibly <zubayer0ahmad@gmail.com>
2026-01-13 19:12:29 +0600

    Remove frontend and public from main

--- frontend/pages/critique/index.ts
+++
@@ -1,5 +0,0 @@
-// src/pages/critique/index.ts - Proper exports for critique pages
-export { default as CritiqueDashboard } from './CritiqueDashboard';
-export { default as SubmitContent } from './SubmitContent';
-export { default as MySubmissions } from './MySubmissions';
-export { default as ProfileSettings } from './ProfileSettings';
